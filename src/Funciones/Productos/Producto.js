6256daf95ea3f044ffa2963322a8ab438ab36715
LaCeBr <cerezo.127529@e.unavarra.es>
2024-04-01 00:47:52 +0200

    Añado botón en lista productos

--- src/Funciones/Productos/Producto.js
+++ src/Funciones/Productos/Producto.js
@@ -1,4 +1,5 @@
 import './Producto.css';
+import Button from 'react-bootstrap/Button';
 
 function Producto(props) {
 
@@ -12,16 +13,18 @@ function Producto(props) {
     const stock = props.producto.stock;
     const precio = props.producto.precio;
     const imagen = props.producto.imagen;
-
+    const cantidad = 100;
     return (
         <div className='producto'>
             <img className='producto__imagen' alt={nombre} src={imagen}/>
             <div className='producto__descripcion'>
-                <div>
-                    <h2>{nombre} - {precio}€/Kg </h2>
-                    <div>Actualmente disponibles: {stock}Kg</div>
-                </div>
+                <h2>{nombre} - {precio}€/Kg </h2>
+                <div>Actualmente disponibles: {stock}Kg</div>
             </div>
+            
+            <Button variant='danger'>+</Button>
+            <p className='producto__cantidad'>{cantidad}g</p>
+            <Button variant='danger'>-</Button>
         </div>
     )
 }
